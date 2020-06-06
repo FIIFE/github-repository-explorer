fd552356acef824ac13529156a66f8bcee750349
Filip Dudziński <filip.dudzinski.90@gmail.com>
2020-06-06 22:46:23 +0200

    Add styles to UsersSearch

--- src/features/users/UsersSearch/UsersSearch.tsx
+++ src/features/users/UsersSearch/UsersSearch.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { useDispatch } from "react-redux";
 import { fetchUsers } from "../usersSlice";
+import styles from "./UsersSearch.module.scss";
 
 export const UsersSearch: React.FC = () => {
   const dispatch = useDispatch();
@@ -8,19 +9,27 @@ export const UsersSearch: React.FC = () => {
 
   const searchSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     dispatch(fetchUsers(searchPhrase));
+    setSearchPhrase("");
     event.preventDefault();
   };
 
   return (
-    <form onSubmit={searchSubmit}>
+    <form onSubmit={searchSubmit} className={styles.form}>
       <input
         type="text"
+        className={styles.input}
+        placeholder="Enter username"
         value={searchPhrase}
         onChange={(event) => {
           setSearchPhrase(event.target.value);
         }}
       />
-      <input type="submit" value="Submit" />
+      <input
+        type="submit"
+        value="Submit"
+        className={styles.button}
+        disabled={!searchPhrase}
+      />
     </form>
   );
 };
